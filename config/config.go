26431161b96cfe16526dfaa92efb3d8e85816786
Taylor <453014399@qq.com>
2017-09-28 20:29:34 +0800

    add config reader

---
+++ config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+type NotifierConfig struct {
+	LogConfigPath string `yaml:"log_config_path"`
+	MySQL MySQL	`yaml:"mysql"`
+	Redis Redis
+}
+
+var notifierConfig NotifierConfig
+
+type MySQL struct {
+	Address string
+	User string
+	Password string
+	DBName string `yaml:"dbname"`
+}
+
+type Redis struct {
+	Address string
+	Password string
+	DB int `yaml:"db"`
+}
+
+func ConfigNotifier() {
+	yamlFile, err := ioutil.ReadFile("notifier_config.yaml")
+	if err != nil {
+		panic("read notifier_config.yaml fail!")
+	}
+
+	err = yaml.Unmarshal(yamlFile, &notifierConfig)
+	if err != nil {
+		panic("unmarshal yamlfile into NotifierConfig fail!")
+	}
+}
