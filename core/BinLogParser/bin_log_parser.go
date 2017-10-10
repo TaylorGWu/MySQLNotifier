d6e5a29cf9d648faedb1897f812bf219fc33c3c2
Taylor <453014399@qq.com>
2017-10-10 18:25:32 +0800

    update BinLogParser

--- core/BinLogParser/bin_log_parser.go
+++ core/BinLogParser/bin_log_parser.go
@@ -1,1 +1,21 @@
 package BinLogParser
+
+import (
+	"MySQLNotifier/util/database/redis"
+)
+
+type BinLogParser struct {
+	RedisUtil *redis.RedisUtil
+}
+
+var Parser BinLogParser
+
+func New() (err error) {
+	err = redis.New()
+	if err != nil {
+		panic("Init BinLogParser fail.\n")
+		return
+	}
+	Parser.RedisUtil = redis.Get()
+	return
+}
